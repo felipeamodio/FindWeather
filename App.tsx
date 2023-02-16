86067d357747562542d988e3347a3fadd8bfe863
Felipe Alves <felipeaamodio@gmail.com>
2023-02-16 10:51:11 -0300

    set up the development ambient

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {useFonts} from 'expo-font';
+
 export default function App() {
+  const [fontLoaded] = useFonts({
+    'Overpass-Light': require('./src/theme/fonts/Overpass-Light.ttf'),
+    'Overpass-Regular': require('./src/theme/fonts/Overpass-Regular.ttf'),
+    'Overpass-Medium': require('./src/theme/fonts/Overpass-Medium.ttf'),
+    'Overpass-Bold': require('./src/theme/fonts/Overpass-Bold.ttf'),
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
